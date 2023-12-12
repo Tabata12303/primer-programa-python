d6ce1a83500d46b8df72a3e730e7fc2deead452f
Tabata12303 <153646980+Tabata12303@users.noreply.github.com>
2023-12-12 11:13:17 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+#Ejercicio 1:Operaciones Básicas y Tipos de Datos
+#suma dos números enters
+a=12
+b=10
+c=(a+b)
+print(c)
+#resta dos números de punto flotante
+d=13.4
+e=20.5
+f=(d-e)
+print(f)
+#multiplica dos números enteros
+g=20
+h=40
+i=(g*h)
+print(i)
+#divide do úmeos de punto flotante
+j=30.9
+k=7.6
+l=(j/k)
+print(l)
+print("En este apartado se realizaron operaciones matemáticas y las imprimimos en la pantalla con print")
+#Ejercicio 2:Manipulación de Cadenas de Texto
+m=("Tabata Decire Gutiérrez Betancourt")
+print(m)
+n=len(m)
+print(n)
+#El próposito de este apartado fue imprimir nuestro nombre con el numero de letras
+#Ejercicio 3:Conversió de Tipos de Datos
+o=40
+p=3.1
+print(o)
+print(float(o))
+print(p)
+print(int(p))
+
+
+
